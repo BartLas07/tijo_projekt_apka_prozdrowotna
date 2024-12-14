694cadafc22f420cfabf90bcaf343c019caa6f0f
Bartosz Lasko <bartek.lasko@wp.pl>
2024-12-14 01:19:13 +0100

    commit testowy 3

--- apka_prozdrowotna_backend_tijo/apka_prozdrowotna/src/test/java/com/example/apka_prozdrowotna/SearchIngredientControllerTest.java
+++ apka_prozdrowotna_backend_tijo/apka_prozdrowotna/src/test/java/com/example/apka_prozdrowotna/SearchIngredientControllerTest.java
@@ -34,6 +34,7 @@ public class SearchIngredientControllerTest {
     @BeforeAll
     void setupDatabase() {
         mealIngredientRepository.deleteAll();
+        mealIngredientRepository.save(new MealIngredient("Tomatoo23"));
         mealIngredientRepository.save(new MealIngredient("Cheese"));
         mealIngredientRepository.save(new MealIngredient("Basil"));
     }
@@ -53,6 +54,7 @@ public class SearchIngredientControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].value", is("Tomatoo23")))
                 .andExpect(jsonPath("$[0].label", is("Tomato")))
                 .andExpect(jsonPath("$[1].value", is("Cheese")))
                 .andExpect(jsonPath("$[1].label", is("Cheese")))
